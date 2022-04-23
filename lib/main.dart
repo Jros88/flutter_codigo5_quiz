10423bd292e23c39678673b9fb3e5be2c894d274
jesusVillegas <gesusr@gmail.com>
2022-04-23 12:23:28 -0500

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,111 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_quiz/quiz_brain.dart';
 
-void main(){
+
+
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: QuizPage(),
-      
     );
   }
 }
 
+class QuizPage extends StatefulWidget {
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
 
-class QuizPage extends StatelessWidget {
-  const QuizPage({ Key? key }) : super(key: key);
+class _QuizPageState extends State<QuizPage> {
+
+
+  QuizBrain matasquita = QuizBrain();
+
+  List<Icon> scoreKeeper = [];
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+      backgroundColor: Color(0xff272C2F),
       appBar: AppBar(
-        title: Text('QuizApp'),
+        backgroundColor: Color(0xff272C2F),
+        title: const Text("QuizApp"),
         centerTitle: true,
       ),
-      
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 5,
+            child: Center(
+              child: Text(
+                matasquita.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                color: const Color(0xff00E1B7),
+                child: const Text("Verdadero"),
+                onPressed: () {
+                  bool correctAnswer = matasquita.getQuestionAnswer();
+                  if (correctAnswer == true) {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Color(0xff00E1B7),
+                      ),
+                    );
+                  } else {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Color(0xfff84073),
+                      ),
+                    );
+                  }
+                  matasquita.nextQuestion();
+                  setState(() {});
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                color: const Color(0xfff84073),
+                child: const Text("False"),
+                onPressed: () {
+
+
+
+
+                },
+              ),
+            ),
+          ),
+          Row(
+            children: scoreKeeper,
+          ),
+        ],
+      ),
     );
   }
 }
